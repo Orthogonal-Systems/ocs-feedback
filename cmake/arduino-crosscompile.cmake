f064e5150e44493a873a5ae1bafc646dd92cd8c9
Matthew Ebert <mfe5003@gmail.com>
2015-12-01 15:14:00 -0600

    moved target definitions to stop repeats

--- cmake/arduino-crosscompile.cmake
+++ cmake/arduino-crosscompile.cmake
@@ -244,49 +244,6 @@ function(add_avr_executable EXECUTABLE_NAME)
       COMMENT "Uploading ${eeprom_image} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
    )
 
-   # get status
-   add_custom_target(
-      get_status_${EXECUTABLE_NAME}
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n -v
-      COMMENT "Get status from ${AVR_MCU}"
-   )
-
-   # get fuses
-   add_custom_target(
-      get_fuses_${EXECUTABLE_NAME}
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n
-         -U lfuse:r:-:b
-         -U hfuse:r:-:b
-         -U efuse:r:-:b
-      COMMENT "Get fuses from ${AVR_MCU}"
-   )
-
-   # set fuses
-   add_custom_target(
-      set_fuses_${EXECUTABLE_NAME}
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U lfuse:w:${AVR_L_FUSE}:m
-         -U hfuse:w:${AVR_H_FUSE}:m
-         -U efuse:w:${AVR_E_FUSE}:m
-         COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE} Extended Fuse: ${AVR_E_FUSE}"
-   )
-
-   # get oscillator calibration
-   add_custom_target(
-      get_calibration_${EXECUTABLE_NAME}
-         ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:r:${AVR_MCU}_calib.tmp:r
-         COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
-   )
-
-   # set oscillator calibration
-   add_custom_target(
-      set_calibration_${EXECUTABLE_NAME}
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:w:${AVR_MCU}_calib.hex
-         COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
-   )
-
    # disassemble
    add_custom_target(
       disassemble_${EXECUTABLE_NAME}
